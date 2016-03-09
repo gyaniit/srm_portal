14ee2683f7ab9e2a4596ff99a80b19f8260632bc
Adarsha Das <adarsha.das.min12@iitbhu.ac.in>
2016-03-10 03:24:23 +0530

    image upload done

--- contact.php
+++ contact.php
@@ -2,14 +2,22 @@
 //require_once 'login.php';
 
   $db_hostname = 'localhost';
-  $db_database = 'u303291028_studb';
-  $db_username = 'u303291028_root';
-  $db_password = 'Md1bpxLRe3';
+  //$db_database = 'u303291028_studb';
+  //$db_username = 'u303291028_root';
+  //$db_password = 'Md1bpxLRe3';
+  $db_database = 'studentdb';
+  $db_username = 'root';
+  $db_password = '';
+
   $db_server = mysql_connect($db_hostname, $db_username, $db_password);
   if($db_server) echo "done connection";
   if (!$db_server) die("Unable to connect to MySQL: " . mysql_error());
   mysql_select_db($db_database)
   or die("Unable to select database: " . mysql_error());
+
+  //print_r($_POST);
+  //print_r($_FILES);
+
 
 
 
@@ -50,6 +58,24 @@
                 echo "INSERT failed: $query<br>" . mysql_error() . "<br><br>";
               }
 
+            }
+
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($branch.$roll.'.jpeg');
+            $uploadOk = 1;
+            $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+            if($_FILES["fileToUpload"]) {
+              $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+              if($check !== false) {
+                  //echo "File is an image - " . $check["mime"] . ".";
+                  $uploadOk = 1;
+                  $_SESSION['is_uploaded'] = $uploadOk;
+                  move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+              } else {
+                  //echo "File is not an image.";
+                  $uploadOk = 0;
+                  $_SESSION['is_uploaded'] = $uploadOk;
+              }
             }
 
 
